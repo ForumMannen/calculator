e50ee68dac32df9bf72c5ad500e1c9a82e96e621
Your name here <lundell.linus@gmail.com>
2022-10-17 18:54:12 +0200

    works fine, looks shit

--- script/script.js
+++ script/script.js
@@ -1,15 +1,31 @@
-const firstField = document.getElementById("num1").value;
-const secondField = document.getElementById("num2").value;
+const select = document.querySelector("select");
+const btn = document.querySelector("button");
+const ul = document.querySelector("ul");
 
-switch (firstField, secondField) {
-    case "addition":
-        break;
-    case "subtraction":
-        break;
-    case "multiplication":
-        break;
-    case "division":
-        break;
-    default:
-        text = "Can't count...";
-}
+btn.addEventListener("click", () => {
+
+    const num1 = parseInt(document.querySelector(".num1").value);
+    const num2 = parseInt(document.querySelector(".num2").value);
+    let sum;
+
+    switch(select.value) {
+        case "+":
+            sum = num1 + num2;
+            break;
+        case "-":
+            sum = num1 - num2;
+            break;
+        case "*":
+            sum = num1 * num2;
+            break;
+        case "/":
+            sum = num1 / num2;
+            break;
+        default:
+            sum = 0;
+    }
+
+    const li = document.createElement("li");
+    li.innerText = sum;
+    ul.appendChild(li);
+});
